b9a38a4bba9bc24460f1607d322c1acf2eb23ccb
smart78 <Artems2311@mail.ru>
2024-06-16 20:10:24 +0300

    Addition config

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,46 @@
-import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:dartssh2/dartssh2.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+Future<Map<String, dynamic>> loadConfig() async {
+  try {
+    final directory = await getApplicationDocumentsDirectory();
+    final file = File('${directory.path}/config.json');
+
+    if (await file.exists()) {
+      String strJson = await file.readAsString();
+      return json.decode(strJson);
+    } else {
+      return {};
+    }
+  } catch (e) {
+    print('Error reading config file: $e');
+    return {};
+  }
+}
+
+Future<void> saveConfig(Map<String, dynamic> config) async {
+  try {
+    final directory = await getApplicationDocumentsDirectory();
+    final file = File('${directory.path}/config.json');
+
+    if (!(await file.exists())) {
+      await file.create();
+    }
+
+    String strJson = json.encode(config);
+    await file.writeAsString(strJson);
+  } catch (e) {
+    print('Error writing config file: $e');
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -30,18 +66,30 @@ class SSHScreen extends StatefulWidget {
 }
 
 class _SSHScreenState extends State<SSHScreen> {
-  final TextEditingController hostController = TextEditingController();
-  final TextEditingController portController = TextEditingController();
-  final TextEditingController usernameController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
-
+  late Map<String, dynamic> config;
   String output = '';
 
+  @override
+  void initState() {
+    super.initState();
+    loadConfig().then((value) {
+      setState(() {
+        config = value;
+      });
+    });
+  }
+
   void connectAndExecute() async {
-    final host = hostController.text;
-    final port = int.tryParse(portController.text) ?? 22;
-    final username = usernameController.text;
-    final password = passwordController.text;
+    final host = config['host'];
+    final port = config['port'];
+    final username = config['username'];
+    final password = config['password'];
+    final path_to_directory = config['path_to_directory'];
+    final open_name = config['open_name'];
+    final password_for_decrypt = config['password_for_decrypt'];
+    final mount_directory = config['mount_directory'];
+
+    print(host);
 
     final client = SSHClient(
       await SSHSocket.connect(host, port),
@@ -50,9 +98,30 @@ class _SSHScreenState extends State<SSHScreen> {
     );
 
     try {
-      final result = await client.run('ls');
+      final shell = await client.shell();
+      final encoder = const Utf8Encoder();
+      final open_directory = 'echo -n "$password_for_decrypt" | sudo cryptsetup luksOpen --key-file - $path_to_directory $open_name\n';
+      final mount = 'sudo mount /dev/mapper/$open_name $mount_directory\n';
+      shell.write(encoder.convert('echo $password | sudo -S clear\n'));
+      shell.write(encoder.convert(open_directory));
+      shell.write(encoder.convert(mount));
+      shell.write(encoder.convert('exit\n'));
+
+      final out = await shell.stdout
+          .cast<List<int>>()
+          .transform(const Utf8Decoder())
+          .join();
+
+      final error = await shell.stderr
+          .cast<List<int>>()
+          .transform(const Utf8Decoder())
+          .join();
+
+      print(out);
+      print(error);
+
       setState(() {
-        output = utf8.decode(result);
+        output = 'The directory is open and ready';
       });
     } catch (e) {
       setState(() {
@@ -61,49 +130,147 @@ class _SSHScreenState extends State<SSHScreen> {
     }
   }
 
+  Future<void> _editConfig() async {
+    Map<String, dynamic>? updatedConfig = await showDialog<Map<String, dynamic>>(
+      context: context,
+      builder: (BuildContext context) {
+        return ConfigDialog(config: config);
+      },
+    );
+    if (updatedConfig != null) {
+      setState(() {
+        config = updatedConfig;
+      });
+      await saveConfig(updatedConfig);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('SSH Example'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+    if (config == null) {
+      return Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('SSH Example'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              ElevatedButton(
+                onPressed: connectAndExecute,
+                child: const Text('Connect and Execute'),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _editConfig,
+                child: const Text('Edit Config'),
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Text(output),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+  }
+}
+
+class ConfigDialog extends StatefulWidget {
+  final Map<String, dynamic> config;
+
+  const ConfigDialog({Key? key, required this.config}) : super(key: key);
+
+  @override
+  _ConfigDialogState createState() => _ConfigDialogState();
+}
+
+class _ConfigDialogState extends State<ConfigDialog> {
+  late Map<String, dynamic> _updatedConfig;
+
+  @override
+  void initState() {
+    super.initState();
+    _updatedConfig = Map.from(widget.config);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('Configure SSH Connection'),
+      content: SingleChildScrollView(
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             TextField(
-              controller: hostController,
-              decoration: const InputDecoration(labelText: 'Host'),
+              decoration: InputDecoration(labelText: 'Host'),
+              onChanged: (value) => _updatedConfig['host'] = value,
+              controller: TextEditingController(text: widget.config['host']),
             ),
             TextField(
-              controller: portController,
+              decoration: InputDecoration(labelText: 'Port'),
+              onChanged: (value) => _updatedConfig['port'] = int.parse(value),
               keyboardType: TextInputType.number,
-              decoration: const InputDecoration(labelText: 'Port'),
+              controller: TextEditingController(text: widget.config['port'].toString()),
             ),
             TextField(
-              controller: usernameController,
-              decoration: const InputDecoration(labelText: 'Username'),
+              decoration: InputDecoration(labelText: 'Username'),
+              onChanged: (value) => _updatedConfig['username'] = value,
+              controller: TextEditingController(text: widget.config['username']),
             ),
             TextField(
-              controller: passwordController,
-              obscureText: true,
-              decoration: const InputDecoration(labelText: 'Password'),
+              decoration: InputDecoration(labelText: 'Password'),
+              onChanged: (value) => _updatedConfig['password'] = value,
+              controller: TextEditingController(text: widget.config['password']),
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: connectAndExecute,
-              child: const Text('Connect and Execute'),
+            TextField(
+              decoration: InputDecoration(labelText: 'Path to directory'),
+              onChanged: (value) => _updatedConfig['path_to_directory'] = value,
+              controller: TextEditingController(text: widget.config['path_to_directory']),
             ),
-            const SizedBox(height: 20),
-            Expanded(
-              child: SingleChildScrollView(
-                child: Text(output),
-              ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Open name'),
+              onChanged: (value) => _updatedConfig['open_name'] = value,
+              controller: TextEditingController(text: widget.config['open_name']),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Password for decrypt'),
+              onChanged: (value) => _updatedConfig['password_for_decrypt'] = value,
+              controller: TextEditingController(text: widget.config['password_for_decrypt']),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Mount directory'),
+              onChanged: (value) => _updatedConfig['mount_directory'] = value,
+              controller: TextEditingController(text: widget.config['mount_directory']),
             ),
           ],
         ),
       ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop(null);
+          },
+          child: Text('Cancel'),
+        ),
+        TextButton(
+          onPressed: () async {
+            Navigator.of(context).pop(_updatedConfig);
+          },
+          child: Text('Save'),
+        ),
+      ],
     );
   }
 }
